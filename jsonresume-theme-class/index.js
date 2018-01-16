18a897f92a843452ca299005e9359d8ddfe1eda8
Nick Scherman <n_scherman@hotmail.com>
2018-01-15 22:31:54 -0500

    Refactor using scss and clean up handlebars templates. Use single "class" theme. WIP.

--- jsonresume-theme-class/index.js
+++ jsonresume-theme-class/index.js
@@ -1,15 +1,20 @@
 var fs = require("fs");
+var path = require('path');
 var Handlebars = require("handlebars");
 
 function render(resume) {
-	var css = fs.readFileSync(__dirname + "/style.css", "utf-8");
-	var template = fs.readFileSync(__dirname + "/resume.template", "utf-8");
-	return Handlebars.compile(template)({
-		css: css,
-		resume: resume
-	});
+    var template = fs.readFileSync(__dirname + "/resume.handlebars", "utf-8");
+    var partials = fs.readdirSync(path.resolve(__dirname, './template-partials'));
+    partials.forEach(partial => {
+       var templatePartial = fs.readFileSync(partial);
+       Handlebars.registerPartial(partial.split('.')[0], templatePartial);
+    });
+
+    return Handlebars.compile(template)({
+        resume: resume
+    });
 }
 
 module.exports = {
-	render: render
+    render: render
 };
