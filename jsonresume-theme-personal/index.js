68029e40de83928144631110d331d733fb79f6e3
Nick Scherman <n_scherman@hotmail.com>
2018-01-17 22:25:49 -0500

    pushing changes so i can use my laptop

--- jsonresume-theme-personal/index.js
+++ jsonresume-theme-personal/index.js
@@ -6,7 +6,8 @@ function render(resume) {
     var template = fs.readFileSync(__dirname + "/resume.handlebars", "utf-8");
     var partials = fs.readdirSync(path.resolve(__dirname, './template-partials'));
     partials.forEach(partial => {
-       var templatePartial = fs.readFileSync(partial);
+        console.log(partial, "PARTIAL");
+       var templatePartial = fs.readFileSync(path.resolve(__dirname, 'template-partials', partial), 'utf-8');
        Handlebars.registerPartial(partial.split('.')[0], templatePartial);
     });
 
